1f278f8a6cbe80ff2f9f9fc9d033d4530bf3deb1
Khushraj Rathod <khushrajrathod@gmail.com>
2020-05-01 00:11:03 +0530

    Add GPL Notice

--- OLDCPP/tictactoe.cpp
+++ OLDCPP/tictactoe.cpp
@@ -1,3 +1,21 @@
+// Copyright 2020 Khushraj Rathod
+//
+// This file is part of TicTacToe.
+//
+// TicTacToe is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// TicTacToe is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with TicTacToe.  If not, see <https://www.gnu.org/licenses/>.
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -139,7 +157,7 @@ void reDrawBoard(bool gameOver) {
      *  7 | 8 | 9
      *    |   |
      *
-     * Player <1 || 2>, Enter a number: 
+     * Player <1 || 2>, Enter a number:
      */
 }
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// Copyright 2020 Khushraj Rathod
+//
+// This file is part of TicTacToe.
+//
+// TicTacToe is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// TicTacToe is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with TicTacToe.  If not, see <https://www.gnu.org/licenses/>.
+
 "use strict";
 
 const grid = document.getElementById("grid").children
